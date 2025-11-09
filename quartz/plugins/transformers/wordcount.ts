0bf80b81408709ba85c93576b8e6d9e6c5e571d2
Will Gibson <william.m.gibson234@gmail.com>
2025-11-09 15:48:44 -0500

    installed word-count module, wrote up function to print out all of the text on each md file in order to test current functionality

--- quartz/plugins/transformers/wordcount.ts
+++ quartz/plugins/transformers/wordcount.ts
@@ -1,7 +1,20 @@
-import { QuartzTransformerPlugin } from "../types"
+import { Root as HTMLRoot } from "hast";
+import { toString } from "hast-util-to-string";
+import count from "word-count";
+import { QuartzTransformerPlugin } from "../types";
 
 export const WordCount: QuartzTransformerPlugin = () => {
     return{
         name: "WordCount",
+        htmlPlugins(){
+            return [
+                () =>{
+                    return async (tree: HTMLRoot,file) =>{
+                        let text = toString(tree);
+                        console.log(text);
+                    }
+                }
+            ]
+        }
     }
 }
