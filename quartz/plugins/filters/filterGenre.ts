b06b3cd00fccffd8e21489826ea2822db696cb4e
Will Gibson <william.m.gibson234@gmail.com>
2025-11-10 10:40:48 -0500

    added functionality to read in genres with new format in the markdown files

--- quartz/plugins/filters/filterGenre.ts
+++ quartz/plugins/filters/filterGenre.ts
@@ -1,32 +1,35 @@
 import { QuartzFilterPlugin } from "../types"
 
 const allowedGenreMap:Record<string,boolean> ={
-    "punk-rock": true,
-    "pop-rock": true,
-    "alt-rock": true,
+    "punk rock": true,
+    "pop rock": true,
+    "alt rock": true,
     "country": true,
-    "german-metal":false,
+    "german metal":false,
 }
 
 
 export const FilterGenre: QuartzFilterPlugin<{}> = () => ({
     name: "FilterGenre",
     shouldPublish(_ctx, [_tree, vfile]) {
-        const tags: string[] = vfile.data?.frontmatter?.tags ?? [];
-        if(tags.length === 0){
+        console.log(`[FilterGenre] File: ${vfile.data?.slug}`);
+        console.log(`[FilterGenre] Frontmatter exists: ${!!vfile.data?.frontmatter}`);
+        console.log(`[FilterGenre] Frontmatter keys: ${vfile.data?.frontmatter ? Object.keys(vfile.data.frontmatter).join(', ') : 'none'}`);
+        console.log(`[FilterGenre] Frontmatter object:`, JSON.stringify(vfile.data?.frontmatter, null, 2));
+        
+        const genres: string[] = (vfile.data?.frontmatter as any)?.genre ?? [];
+        console.log(`[FilterGenre] Genres array:`, genres);
+        if(genres.length === 0){
+            console.log(`No tags found for ${vfile.data?.frontmatter?.title}, publishing`);
             return true;
         }
-        for (const tag of tags) {
-            if(tag.startsWith("genre: ")){
-                if (allowedGenreMap[tag.split(":")[1]]) {
-                    return true;
-                } 
-                else{
-                    return false;
-                }
-            }
-            else{
+        for (const genre of genres) {
+            console.log(`Checking genre: ${genre}`);
+            if (allowedGenreMap[genre]) {
                 return true;
+            } 
+            else{
+                    return false;
             }
         }
         return false;
