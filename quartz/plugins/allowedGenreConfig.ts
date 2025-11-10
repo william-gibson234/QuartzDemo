932b7409eeac290da9a525ecf229ad708aa6d08c
Will Gibson <william.m.gibson234@gmail.com>
2025-11-10 13:00:45 -0500

    updated genre config to include function to get the genres in a file

--- quartz/plugins/allowedGenreConfig.ts
+++ quartz/plugins/allowedGenreConfig.ts
@@ -1,3 +1,5 @@
+import { VFile } from "vfile"
+
 export const allowedGenreMap:Record<string,boolean> = {
     "punk rock": true,
     "pop rock": true,
@@ -5,3 +7,6 @@ export const allowedGenreMap:Record<string,boolean> = {
     "country": true,
     "german metal":false,
 }
+
+export const genresFromFrontmatter = (vfile: VFile): string[] => 
+    ((vfile.data?.frontmatter as any)?.genre ?? [])
